15d2b5ecec6f40348da09e15b4059abbd287f8e8
cardinal <caren@merzhaus.org>
2022-09-01 17:59:13 -0400

    setting up front end

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
   type User {
@@ -46,6 +46,6 @@ const typeDefs = gql`
     addReaction(thoughtId: ID!, reactionBody: String!): Thought
     addFriend(friendId: ID!): User
   }
-`;
+`
 
-module.exports = typeDefs;
+module.exports = typeDefs
